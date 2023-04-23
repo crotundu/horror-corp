e1ae9c600037c4f19715a45d9cbad88f4b3f3b53
Cristian Rotundu <cristian.rotundu@gmail.com>
2023-04-23 12:50:57 +0300

    Add a GO backend

---
+++ apps/api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/health", func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*");
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router.Run(":" + port)
+}
