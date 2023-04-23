6a206e4e83624081dc6d78cdf771b6798dcf673a
Cristian Rotundu <cristian.rotundu@gmail.com>
2023-04-23 12:41:37 +0300

    Remove 'About' MFE for simplicity

--- apps/about/module-federation.config.js
+++
@@ -1,9 +0,0 @@
-const baseConfig = require('../../module-federation-base.config');
-
-module.exports = {
-  ...baseConfig,
-  name: "about",
-  exposes: {
-    "./Module": "./src/remote-entry.ts",
-  },
-};
